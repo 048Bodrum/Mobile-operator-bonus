e2bd2c96f689f85b639a9085847e4cf366edc74a
Bilen Liubov <bilenliubov48@mail.ru>
2024-09-10 15:30:31 +0300

    New project

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int initialBalance = 600;
+        int addend = 1500;
+
+        int bonus = 0;
+        int finalBalance;
+        if (addend > 1000) {
+            bonus = addend / 100;
+            finalBalance = initialBalance + addend + bonus;
+        } else {
+            bonus = 0;
+            finalBalance = initialBalance + addend;
+        }
+            System.out.println("Итоговый счет: " + finalBalance);
+            System.out.println("Бонусы: " + bonus);
+    }
+}
